072d6f8450d7464246ed57ec9c1d63953d813c59
carlos gabriel <carlosgabriel71@yahoo.com>
2016-09-23 00:27:01 -0700

    feat: Add all() method

--- js/funnel.js
+++ js/funnel.js
@@ -62,7 +62,39 @@
             },
 
             // class methods
-            "methods__": {},
+            "methods__": {
+                /**
+                 * @description [Gets all elements from source point.]
+                 * @param  {Array}  source [A source point element contained in an array. **Source parameter
+                 *                          is only present when running the constructor. Chaining methods
+                 *                          does not provide the source parameter. Thus allowing the method to
+                 *                          be chainable..]
+                 * @return {Array|Object}  [Return elements array if invoked from constructor. Otherwise return
+                 *                          self to allow method chaining.]
+                 */
+                "all": function(source) {
+
+                    // define vars
+                    var elements = [],
+                        this_ = this,
+                        l = (source) ? source.length : this_.length,
+                        array = (source) ? source : this_.stack[this_.stack.length - 1];
+
+                    // loop through source and get all its elements
+                    for (var i = 0; i < l; i++) {
+                        elements = elements.concat(to_array(array[i].getElementsByTagName("*")));
+                    }
+
+                    // only returns for constructor
+                    if (source) return elements;
+
+                    // add elements to selector object
+                    this_.stack.push(elements);
+                    this_.length = elements.length;
+                    return this_;
+
+                }
+            },
 
             // class to extend
             "extend__": false
