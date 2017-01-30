d4490fd2ebb0d88112d388ffe5d69f09a6e11176
carlos gabriel <carlosgabriel71@yahoo.com>
2017-01-30 09:49:28 -0800

    refactor: Update app.js
    Update app.js with the new methods. The pop() method was replaced
    with the new getStack/getElement methods.

--- js/app.js
+++ js/app.js
@@ -13,14 +13,14 @@ document.onreadystatechange = function() {
         // <source_point(s)>.<filter1>.<filter2>.<filtern>.<pop>;
 
         // example...
-        // f("#aside:all").tags("span", "div").pop();
+        // f("#aside:all").tags("span", "div").getStack();
 
         // query explanation...
         // 1) query uses the element with the id of "aside" as its source point*
         //    the ":all" means to grab ALL its descendants elements
         // 2) now that we have all the descendants, we use the .tags() filter
         //    to only get elements with the tag-type of "span" and "div"
-        // 3) finally, return the collection with the pop() method
+        // 3) finally, return the collection with the getStack() method
 
         // *What is a source point?
         // A source point is a element in which the query will focus on.
@@ -31,24 +31,24 @@ document.onreadystatechange = function() {
         // example queries...
 
         // 1) uses the elements with id "tape" and "file" as source points
-        // 2) no filters are applied. therefore invoking pop() will only
+        // 2) no filters are applied. therefore invoking getStack() will only
         //    return the provided source elements
-        f("#tape", "#file").pop();
+        f("#tape", "#file").getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) of the descendants filter out any that contain the class "active"
-        // 3) return the collection for use with pop()
-        f("#red:all").classes("!active").pop();
+        // 3) return the collection for use with getStack()
+        f("#red:all").classes("!active").getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) get form type elements of the type checkbox
         // 3) state(), in this case, will only return non checked elements
-        // 4) return the collection for use with pop()
-        f("#red:all").form(":checkbox").state("checked", false).pop();
+        // 4) return the collection for use with getStack()
+        f("#red:all").form(":checkbox").state("checked", false).getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
@@ -56,8 +56,8 @@ document.onreadystatechange = function() {
         // 2) get form type elements of the type checkbox
         // 3) state(), in this case, will only return non checked elements
         // 4) state(), in this case, will only return non visible elements
-        // 5) return the collection for use with pop()
-        f("#red:all").form(":checkbox").state("checked", false).state("visible", false).pop();
+        // 5) return the collection for use with getStack()
+        f("#red:all").form(":checkbox").state("checked", false).state("visible", false).getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
@@ -65,62 +65,62 @@ document.onreadystatechange = function() {
         // 2) get form type elements of the type checkbox
         // 3) state(), in this case, will only return non checked elements
         // 4) state(), in this case, will only return visible elements
-        // 5) return the collection for use with pop()
-        f("#red:all").form(":checkbox").state("checked", false).state("visible", true).pop();
+        // 5) return the collection for use with getStack()
+        f("#red:all").form(":checkbox").state("checked", false).state("visible", true).getStack();
 
         // 1) uses the element with id "pink" as a source point.
         // 2) get the source point elements siblings
-        // 3) return the collection for use with pop()
-        f("#pink").siblings().pop();
+        // 3) return the collection for use with getStack()
+        f("#pink").siblings().getStack();
 
         // 1) uses the element with id "red" as a source point.
         // 2) get the source points parent
-        // 3) return the collection for use with pop()
-        f("#red").parent().pop();
+        // 3) return the collection for use with getStack()
+        f("#red").parent().getStack();
 
         // 1) uses the element with id "red" as a source point.
         // 2) get the source points parents
-        // 3) return the collection for use with pop()
-        f("#red").parents().pop();
+        // 3) return the collection for use with getStack()
+        f("#red").parents().getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) use the tag() filter to return elements of tag-type "input"
-        // 3) return the collection for use with pop()
-        f("#red:all").tags("input").pop();
+        // 3) return the collection for use with getStack()
+        f("#red:all").tags("input").getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) use the tag() filter to return elements of tag-type "form"
         // 3) get the children of the collection
-        // 4) return the collection for use with pop()
-        f("#red:all").tags("form").children().pop();
+        // 4) return the collection for use with getStack()
+        f("#red:all").tags("form").children().getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) get the children of the collection
         // 3) use the attrs() filter to return elements with the attribute "type=file"
-        // 4) return the collection for use with pop()
-        f("#red:all").children().attrs("'type=file'").pop();
+        // 4) return the collection for use with getStack()
+        f("#red:all").children().attrs("'type=file'").getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) get the children of the collection
         // 3) use the attrs() filter to return elements with the attribute "class" (the attribute value in this query is not checked, simply checks if the element HAS the attribute)
-        // 4) return the collection for use with pop()
-        f("#red:all").children().attrs("'class'").pop();
+        // 4) return the collection for use with getStack()
+        f("#red:all").children().attrs("'class'").getStack();
 
         // 1) uses the element with id "tape" as a source point.
         //    using :all gets ALL the elements descendants rather than the
         //    the element itself
         // 2) get the children of the collection
         // 3) use the tag() filter to return elements of tag-type "input" or "canvas"
-        // 4) return the collection for use with pop()
-        f("#red:all").children().tags("input", "canvas").pop();
+        // 4) return the collection for use with getStack()
+        f("#red:all").children().tags("input", "canvas").getStack();
 
     }
 
