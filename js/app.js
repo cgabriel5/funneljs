48c2e196c2bcccb5266517f6cc3e792de4908c68
cgabriel5 <carlosgabriel71@yahoo.com>
2017-08-09 18:30:26 -0700

    refactor: Update test.js
    Update the test.js examples to match that of the README.md examples.

--- js/app.js
+++ js/app.js
@@ -986,116 +986,97 @@ document.onreadystatechange = function() {
         // get the library
         var libs = app.libs,
             f = libs.Funnel;
-        // example queries...
-        // 1) uses the elements with id "tape" and "file" as source points
-        // 2) no filters are applied. therefore invoking getStack() will only
-        //    return the provided source elements
+        // Examples...
+        // - Uses the elements with id "tape" and "file" as source points.
+        // - No filters are applied. Therefore invoking "getStack" will only return the provided source elements
         f("#tape", "#file")
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) of the descendants filter out any that contain the class "active"
-        // 3) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Of the descendants filter out any that contain the class "active".
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .classes("!active")
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) get form type elements of the type checkbox
-        // 3) state(), in this case, will only return non checked elements
-        // 4) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Get form type elements of the type checkbox.
+        // - "state", in this case, will only return non checked elements.
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .form(":checkbox")
             .state("checked", false)
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) get form type elements of the type checkbox
-        // 3) state(), in this case, will only return non checked elements
-        // 4) state(), in this case, will only return non visible elements
-        // 5) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Get form type elements of the type checkbox.
+        // - "state", in this case, will only return non checked elements.
+        // - "state", in this case, will only return non visible elements.
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .form(":checkbox")
             .state("checked", false)
             .state("visible", false)
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) get form type elements of the type checkbox
-        // 3) state(), in this case, will only return non checked elements
-        // 4) state(), in this case, will only return visible elements
-        // 5) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Get form type elements of the type checkbox.
+        // - "state", in this case, will only return non checked elements.
+        // - "state", in this case, will only return visible elements.
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .form(":checkbox")
             .state("checked", false)
             .state("visible", true)
             .getStack();
-        // 1) uses the element with id "pink" as a source point.
-        // 2) get the source point elements siblings
-        // 3) return the collection for use with getStack()
+        // - Uses the element with id "pink" as a source point.
+        // - Get the source point elements siblings.
+        // - Return the collection for use with "getStack".
         f("#pink")
             .siblings()
             .getStack();
-        // 1) uses the element with id "red" as a source point.
-        // 2) get the source points parent
-        // 3) return the collection for use with getStack()
+        // - Uses the element with id "red" as a source point.
+        // - Get the source points parent.
+        // - Return the collection for use with "getStack".
         f("#red")
             .parent()
             .getStack();
-        // 1) uses the element with id "red" as a source point.
-        // 2) get the source points parents
-        // 3) return the collection for use with getStack()
+        // - Uses the element with id "red" as a source point.
+        // - Get the source points parents.
+        // - Return the collection for use with "getStack".
         f("#red")
             .parents()
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) use the tag() filter to return elements of tag-type "input"
-        // 3) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Use the "tag" filter to return elements of tag-type "input".
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .tags("input")
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) use the tag() filter to return elements of tag-type "form"
-        // 3) get the children of the collection
-        // 4) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Use the "tag" filter to return elements of tag-type "form".
+        // - Get the children of the collection.
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .tags("form")
             .children()
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) get the children of the collection
-        // 3) use the attrs() filter to return elements with the attribute "type=file"
-        // 4) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Get the children of the collection.
+        // - Use the "attrs" filter to return elements with the attribute "type=file".
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .children()
-            .attrs("'type=file'")
+            .attrs("[type=file]")
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) get the children of the collection
-        // 3) use the attrs() filter to return elements with the attribute "class" (the attribute value in this query is not checked, simply checks if the element HAS the attribute)
-        // 4) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Get the children of the collection.
+        // - Use the "attrs" filter to return elements with the attribute "class" (the attribute value in this query is not checked, simply checks if the element HAS the attribute).
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .children()
-            .attrs("'class'")
+            .attrs("[class]")
             .getStack();
-        // 1) uses the element with id "tape" as a source point.
-        //    using :all gets ALL the elements descendants rather than the
-        //    element itself
-        // 2) get the children of the collection
-        // 3) use the tag() filter to return elements of tag-type "input" or "canvas"
-        // 4) return the collection for use with getStack()
+        // - Uses the element with id "tape" as a source point. Using ":all" gets ALL the elements descendants rather than the element itself.
+        // - Get the children of the collection.
+        // - Use the "tag" filter to return elements of tag-type "input" or "canvas".
+        // - Return the collection for use with "getStack".
         f("#red:all")
             .children()
             .tags("input", "canvas")
